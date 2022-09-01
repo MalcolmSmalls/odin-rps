9c74569e9b064e6ae4ea67fc1552a1a24437d9d6
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-31 18:25:58 -0700

    Finish conditional for RPS

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ const getComputerChoice = () => {
 
 console.log(getComputerChoice())
 
+const playRound = (playerSelection, computerSelection) => {
+    let choice = playerSelection.toLowerCase()
+    let cpuResults = computerSelection.substring(0,1).toUpperCase() + computerSelection.substr(1)
+    let playerResults = playerSelection.substring(0,1).toUpperCase() + playerSelection.substr(1)
+    if(choice === computerSelection){
+        return `Draw! Both chose ${choice}.`
+    }else if(choice === 'rock' && computerSelection === 'scissors' ||
+    choice === 'scissors' && computerSelection === 'paper' ||
+    choice === 'paper' && computerSelection === 'rock'){
+        return `You win! ${playerResults} beats ${computerSelection}.`
+    }else{
+        return `You lose! ${cpuResults} beats ${choice}.`
+    }
+}
+
+console.log(playRound('paper','rock'))
