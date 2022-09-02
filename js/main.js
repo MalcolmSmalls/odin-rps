5ad02ef195974bb08ce9673ab73c42fe58fc3dda
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 09:56:27 -0700

    Fix CPU not randomizing, fix results not clearing after win

--- js/main.js
+++ js/main.js
@@ -10,20 +10,39 @@ const getComputerChoice = () => {
     }
 }
 
-let cpuChoice = getComputerChoice()
+// let cpuChoice = getComputerChoice()
 
 const displayedResultsDiv = document.querySelector('.textHolder') 
+const displayedScoreDiv = document.querySelector('.score')
 const buttons = document.querySelectorAll('button')
+const displayedWinnerDiv = document.querySelector('.results')
 
 let playerChoice
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playerChoice= button.value
-        displayedResultsDiv.textContent = playRound(playerChoice, cpuChoice)
-    })
-})
+    button.addEventListener('click', function() {
+        playerChoice = button.value
+        displayedResultsDiv.textContent = playRound(playerChoice, getComputerChoice())
+        playGame()})
+});
 
+let cpuCount = 0
+let playerCount = 0
 
+const playGame = () => {
+    displayedScoreDiv.textContent = `CPU: ${cpuCount} 
+    You: ${playerCount}`
+    if(playerCount === 5){
+        displayedWinnerDiv.textContent = "You Win! Congratulations!"
+        playerCount = 0
+        cpuCount = 0
+    }else if(cpuCount === 5){
+        displayedWinnerDiv.textContent = "You lose! Play again."
+        playerCount = 0
+        cpuCount = 0
+    }else{
+        displayedWinnerDiv.textContent = ""
+    }
+}
 
 const playRound = (playerSelection, computerSelection) => {
     let choice = playerSelection.toLowerCase()
@@ -34,11 +53,17 @@ const playRound = (playerSelection, computerSelection) => {
     }else if(choice === 'rock' && computerSelection === 'scissors' ||
     choice === 'scissors' && computerSelection === 'paper' ||
     choice === 'paper' && computerSelection === 'rock'){
+        playerCount++
         return `You win! ${playerResults} beats ${computerSelection}.`
     }else{
+        cpuCount++
         return `You lose! ${cpuResults} beats ${choice}.`
     }
 }
+
+
+
+
 
 // const playGame = function(){
 //     let playerChoice = prompt('What is your choice?')
